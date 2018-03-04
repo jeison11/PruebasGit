01ea72126bdd5f3c0744ee148b8c9a2e85871e6c
maik101010 <maiksoft10@gmail.com>
2018-03-04 00:07:22 -0500

    cambio en archivo principal

--- src/carpeta/Principal.java
+++ src/carpeta/Principal.java
@@ -12,6 +12,7 @@ package carpeta;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Y Messi es mejor que penaldo");
     }
     
 }
